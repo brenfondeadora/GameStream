c75b1da485691e4b37e9b9b0639c8d27e56dabff
Brenda SC <brenda.saavedra@fondeadora.com>
2023-03-23 08:45:24 -0600

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Brenda Saavedra  on 23/03/23.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
