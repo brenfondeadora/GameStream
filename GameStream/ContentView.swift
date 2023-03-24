3298e4104869566577baa500d31f518faa886621
Brenda SC <brenda.saavedra@fondeadora.com>
2023-03-23 18:44:07 -0600

    feat: initView

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -9,18 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 19/255, green: 30/255, blue: 53/255).ignoresSafeArea()
+            VStack{
+                Image("AppLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250)
+                InitialView()
+            }
         }
-        .padding()
     }
 }
 
+struct InitialView: View {
+    
+    enum ScreenType {
+        case login, signIn
+    }
+    
+    @State var screen:ScreenType = .login
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Spacer()
+                Button("INICIA SESIÓN"){
+                    screen = .login
+                }.foregroundColor(screen == .login ? .white : .gray)
+                Spacer()
+                Button("REGISTRO") {
+                    screen = .signIn
+                }.foregroundColor(screen == .signIn ? .white : .gray)
+                Spacer()
+            }
+            Spacer()
+            if(screen == .login){
+                LoginView()
+            }else{
+                SignInView()
+            }
+            Spacer()
+        }.padding([.top], 30)
+    }
+}
+
+struct LoginView: View {
+    var body: some View {
+        Text("LoginView")
+    }
+}
+
+struct SignInView: View {
+    var body: some View {
+        Text("SignInView")
+    }
+}
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        Image("pantalla1").resizable()
         ContentView()
     }
 }
