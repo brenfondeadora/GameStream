4eebf43f13c2bb3b7a262e9053e428e9e98eaa55
Brenda SC <brenda.saavedra@fondeadora.com>
2023-03-25 11:15:08 -0600

    style: add header to screen

--- GameStream/Views/SignInView.swift
+++ GameStream/Views/SignInView.swift
@@ -10,6 +10,28 @@ import SwiftUI
 
 struct SignInView: View {
     var body: some View {
-        Text("SignInView")
+        ScrollView{
+            VStack(alignment: .center){
+                Text("Elije una foto de perfíl")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    
+                Text("Puedes cambiar o elejirla más adelante")
+                    .font(.footnote)
+                    .fontWeight(.light)
+                    .foregroundColor(Color.white)
+                    .padding(.bottom)
+                
+                Button(action: takePhoto, label: {
+                    ZStack{
+                        Image("perfilEjemplo").resizable().aspectRatio(contentMode: .fit).frame(width: 80.0, height: 80.0)
+                        
+                        Image(systemName: "camera").foregroundColor(.white)
+                    }
+                })
+            }.padding(.top, 30)
+        }
     }
+    
+    func takePhoto()  { }
 }
