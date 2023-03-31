a04c247340043f1d4a052fce0adfcc85bafaed8a
Brenda SC <brenda.saavedra@fondeadora.com>
2023-03-31 13:33:27 -0600

    style: design full screen

--- GameStream/Views/SignInView.swift
+++ GameStream/Views/SignInView.swift
@@ -30,8 +30,40 @@ struct SignInView: View {
                     }
                 })
             }.padding(.top, 30)
+            
+            VStack(alignment: .leading){
+                CustomTextField(title: "Correo electrónico *", placeholder: "email@gmail.com", keyboardType: .emailAddress)
+                CustomTextField(title: "Contraseña *", placeholder: "12345", keyboardType: .default, isSecureField: true)
+                CustomTextField(title: "Confirmar contraseña *", placeholder: "12345", keyboardType: .default, isSecureField: true)
+                Button(action: signIn) {
+                    Text("REGÍSTRATE")
+                }.buttonStyle(PrimaryButtonStyle())
+                .padding(.horizontal, 30)
+                
+                Text("Regístrate con redes sociales").frame( maxWidth: .infinity,  alignment: .center).foregroundColor(.white).padding(.top, 30)
+                                
+                
+                HStack{
+                    Button(action: facebook) {
+                       Text("FACEBOOK")
+                            .frame(maxWidth: .infinity)
+                    }.buttonStyle(SecondaryButtonStyle())
+                    Spacer()
+                    Button(action: twitter) {
+                        Text(" TWITTER")
+                            .frame(maxWidth: .infinity)
+                    }.buttonStyle(SecondaryButtonStyle())
+                }.padding(.horizontal, 30)
+            }.padding(.top, 30)
+            
         }
     }
     
     func takePhoto()  { }
+    
+    func signIn() { }
+    
+    func facebook() { }
+    
+    func twitter() { }
 }
